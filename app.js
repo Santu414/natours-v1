6ba719f8a9b7c8a5ecacd3735936ec4d184b2a32
Santu414 <snagankera@gmail.com>
2024-07-22 15:05:06 +0530

    NATR-10 - Refactored the code

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const fs = require("fs")
 
 //Read data from json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
+// 1) MIDDLEWARE
 // Middleware for parsing JSON and URL-encoded request bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"))
 
-//Initial server request
-app.get("/",(req,res)=>{
-    res.status(200).json({Message:"Hello from server side 123456789 "})
-})
-
-app.post("/",(req,res)=>{
-    res.status(200).json({Message:"You can post the data"})
-})
-
+// 2) ROUTE HANDLERs
 //GET all tours
-app.get("/api/v1/tours/", (req, res) => {
-    if(tours.length === 0){
-        res.status(404).json({
-            "status":"Not Found",
-            "Message":`There is no data found for given ${id}`
-        })
-  }
-  res.json({
-    status: "Succes",
-    message: "List of all tours",
-    count: tours.length,
-    data: tours,
-  });
+const getAllTours = (req, res) => {
+  if(tours.length === 0){
+      res.status(404).json({
+          "status":"Not Found",
+          "Message":`There is no data found for given ${id}`
+      })
+}
+res.json({
+  status: "Succes",
+  message: "List of all tours",
+  count: tours.length,
+  data: tours,
 });
+}
 
 // Get single tour based on ID
-app.get("/api/v1/tours/:id", (req, res) => {
+const getTour = (req, res) => {
   const id = Number(req.params.id);
   const results = tours.filter((item) => item.id == id);
   if(results.length === 0){
@@ -50,10 +44,10 @@ app.get("/api/v1/tours/:id", (req, res) => {
     count: results.length,
     data: results,
   });
-});
+}
 
-// Create new tour
-app.post("/api/v1/tours/", (req, res) => {
+// Create New Tour
+const createTour =  (req, res) => {
   let reqBody = req.body;
   const isTourExist = tours.filter((item) => item.id == reqBody.id);
   if (isTourExist.length > 0) {
@@ -73,12 +67,13 @@ app.post("/api/v1/tours/", (req, res) => {
       data:newTour
     });
   }
-});
+}
 
 // Update Exesting tours
-app.put("/api/v1/tours/", (req, res) => {
+const updateTour = (req, res) => {
+  const id = req.params.id;
   const reqBody = req.body;
-  const isTourExist = tours.filter((item) => item.id == reqBody.id);
+  const isTourExist = tours.filter((item) => item.id == id);
 
   if (isTourExist.length == 0) {
     res.json({ message: "There is no tour to update" });
@@ -99,23 +94,33 @@ app.put("/api/v1/tours/", (req, res) => {
       data: updateTour,
     });
   }
-});
+}
 
 // Delete Tour
-app.delete("/api/v1/tours/:id",(req,res)=>{
-    const id = Number(req.params.id)
-    const newTour = tours.filter(item=>item.id!==id);
-    fs.writeFileSync(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(newTour, null, 2),
-      "utf-8"
-    );
-    res.json({
-      status: "Success",
-      message: "The tour has been Deleted successfully",
-    });
-})
+const deleteTour = (req,res)=>{
+  const id = Number(req.params.id)
+  const newTour = tours.filter(item=>item.id!==id);
+  fs.writeFileSync(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(newTour, null, 2),
+    "utf-8"
+  );
+  res.json({
+    status: "Success",
+    message: "The tour has been Deleted successfully",
+  });
+}
+//app.get("/api/v1/tours/", getAllTours);
+//app.get("/api/v1/tours/:id",getTour );
+//app.post("/api/v1/tours/",createTour);
+//app.put("/api/v1/tours/:id", updateTour);
+//app.delete("/api/v1/tours/:id",deleteTour);
 
+// 3) ROUTES
+app.route("/api/v1/tours/").get(getAllTours).post(createTour);
+app.route("/api/v1/tours/:id").get(getTour).put(updateTour).delete(deleteTour)
+
+// 4) START SERVER
 const port = 3000;
 app.listen(port,()=>{
     console.log(`App is running on port ${port}....`)
