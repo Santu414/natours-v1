da0ef782e6fdd413774169bcf0ee06065b30d4d3
Santu414 <snagankera@gmail.com>
2024-07-23 23:35:16 +0530

    NATR-15 - Implemented Tours using mongoose and mongoDB

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,17 +1,10 @@
 const fs = require("fs");
+const Tours = require("../models/ToursModel")
 //Read data from json file
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
 
 //GET all tours  
-const getAllTours = (req, res) => {
-  if (tours.length === 0) {
-    res.status(404).json({
-      status: "Not Found",
-      Message: `There is no data found for given ${id}`,
-    });
-  }
+const getAllTours = async (req, res) => {
+  const tours = await Tours.find()
   res.json({
     status: "Succes",
     message: "List of all tours",
@@ -21,82 +14,40 @@ const getAllTours = (req, res) => {
 };
 
 // Get single tour based on ID
-const getTour = (req, res) => {
-  const id = Number(req.params.id);
-  const results = tours.filter((item) => item.id == id);
-  if (results.length === 0) {
-    res.status(404).json({
-      status: "Not Found",
-      Message: `There is no data found for given ${id}`,
-    });
-  }
+const getTour = async (req, res) => {
+  const tour = await Tours.findById(req.params.id);
   res.json({
-    status: "Succes",
-    message: "Single tours",
-    count: results.length,
-    data: results,
+    status: "Success",
+    message: `Getting Tour by ${req.params.id}`,
+    data: tour,
   });
 };
 
 // Create New Tour
-const createTour = (req, res) => {
-  let reqBody = req.body;
-  const isTourExist = tours.filter((item) => item.id == reqBody.id);
-  if (isTourExist.length > 0) {
-    return res.status(400).json({ message: "The Tour already exists" });
-  } else {
-    const newId = tours.length;
-    const newTour = { id: newId, ...reqBody };
-    tours.push(newTour);
-    fs.writeFileSync(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(tours, null, 2),
-      "utf-8"
-    );
-    res.json({
-      status: "Success",
-      message: "The tour has been created successfully",
-      data: newTour,
-    });
-  }
+const createTour = async (req, res) => {
+ const tours = await Tours.create(req.body);
+  res.json({
+    status: "Success",
+    message: "The tour has been created successfully",
+    data: tours,
+  });
 };
+
 
 // Update Exesting tours
-const updateTour = (req, res) => {
-  const id = req.params.id;
-  const reqBody = req.body;
-  const isTourExist = tours.filter((item) => item.id == id);
-
-  if (isTourExist.length == 0) {
-    res.json({ message: "There is no tour to update" });
-  } else {
-    const isUpdateTour = tours.filter((item) => item.id !== reqBody.id);
-    const tourId = isTourExist[0].id;
-    const updateTour = { id: tourId, ...reqBody };
-
-    isUpdateTour.push(updateTour);
-    fs.writeFileSync(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(isUpdateTour, null, 2),
-      "utf-8"
-    );
-    res.json({
-      status: "Success",
-      message: "The tour has been updated successfully",
-      data: updateTour,
-    });
-  }
+const updateTour = async(req, res) => {
+  const updateTour = await Tours.findByIdAndUpdate(req.params.id,req.body,{ new: true })
+  res.json({
+    status: "Success",
+    message: "The tour has been updated successfully",
+    data: updateTour,
+  });
 };
 
+
 // Delete Tour
-const deleteTour = (req, res) => {
-  const id = Number(req.params.id);
-  const newTour = tours.filter((item) => item.id !== id);
-  fs.writeFileSync(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(newTour, null, 2),
-    "utf-8"
-  );
+const deleteTour = async (req, res) => {
+  const deleteTour = await Tours.findByIdAndDelete(req.params.id)
   res.json({
     status: "Success",
     message: "The tour has been Deleted successfully",
---
+++ models/ToursModel.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"]
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"]
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group size"]
+    },
+    difficulty: {
+      type: String
+    },
+    ratingsAverage: {
+      type: Number
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"]
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description"]
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image"]
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date],
+  
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+
+const Tour = mongoose.model("Tours", tourSchema);
+
+module.exports =  Tour;
