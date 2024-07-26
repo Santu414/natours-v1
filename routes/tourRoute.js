73ece0daf13856c9918f8457ec726d6c748af469
dileepnagankera <dileepnagankera@gmail.com>
2024-07-26 19:56:18 +0530

    NATR-22 -Code Implemented Don For Tour Stats

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -7,9 +7,11 @@ const {
   updateTour,
   deleteTour,
   getTop5Tours,
+  getTourStats,
 } = require("../controllers/toursController");
 router.get("/", getAllTours);
 router.get("/top-5-tours", getTop5Tours);
+router.get("/tour-stats", getTourStats);
 router.get("/:id", getTour);
 router.post("/", createTour);
 router.put("/:id", updateTour);
