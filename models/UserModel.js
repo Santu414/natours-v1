7b96eacfc204354b4770c2af0fb727188c9a2e96
Santu414 <snagankera@gmail.com>
2024-07-28 20:12:16 +0530

    NATR-23 - Validated password and hashed it.

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+var validator = require("validator");
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -7,24 +9,39 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: [true, "Please provide your email"],
+    required: [true, "Email is required"],
     unique: true,
     lowercase: true,
-   
+    validate: [validator.isEmail
+      , "Please enter valid email ID"]
   },
-  photo: {
-    type: String,
-    default: "default.jpg",
-  },
-  role: {
-    type: String,
-    enum: ["user", "guide", "lead-guide", "admin"],
-    default: "user",
-  },
+  photo: String,
   password: {
-    type: String
+    type: String,
+    required: [true, "Password is required"],
   },
- 
+  passwordConfirm: {
+    type: String,
+    required: [true, "COnfirm Password is required"],
+  },
+});
+
+// Pre-save hook to hash the password
+userSchema.pre('save', async function(next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) return next();
+  try {
+      // Generate a salt
+      const salt = await bcrypt.genSalt(10);
+      // Hash the password using the salt
+      this.password = await bcrypt.hash(this.password, salt);
+
+      // Delete confirm password
+      this.passwordConfirm = undefined
+      next();
+  } catch (error) {
+      next(error);
+  }
 });
 
 const User = mongoose.model("Users", userSchema);
