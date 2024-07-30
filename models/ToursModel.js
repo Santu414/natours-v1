56d720035361854cea105a89f413b89977979081
dileepnagankera <dileepnagankera@gmail.com>
2024-07-30 11:47:37 +0530

    NATR-29 - Query Middleware

--- models/ToursModel.js
+++ models/ToursModel.js
@@ -53,6 +53,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour:{
+      type:Boolean,
+      default:false,
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -73,6 +77,21 @@ tourSchema.pre("save", function (next) {
  // next();/
 //});
 
+
+//Query Middleware
+//tourSchema.pre("find", function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+  next();
+});
+ 
 
 const Tour = mongoose.model("Tours", tourSchema);
 
