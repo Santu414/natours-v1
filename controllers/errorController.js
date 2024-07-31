5f64c0b9d6fddab7536f2819d518eda4db5fb7ea
dileepnagankera <dileepnagankera@gmail.com>
2024-07-31 16:09:06 +0530

    NATR-41 -  Handling Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,14 @@
 const AppError = require("./../utils/appError");
 
-const handleCastErrorBD = (err) => {
+const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}:${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/);
+  console.log("value", value);
+  const message = `Duplicate field value: ${value} Please use another value`;
   return new AppError(message, 400);
 };
 
@@ -43,7 +50,8 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
-    if (error.name === "CastError") error = handleCastErrorBD(error);
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     sendErrorProd(error, res);
   }
 };
