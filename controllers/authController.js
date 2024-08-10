25ceab7f605a26765c859b88a126dd4a538045fa
dileepnagankera <dileepnagankera@gmail.com>
2024-08-10 17:38:24 +0530

    NATR-48 Protecting Tour Routes - Part 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,8 @@
-const jwt =require("jsonwebtoken")
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
 const User = require("./../models/UserModel");
 const catchAsync = require("./../utils/catchAsync");
-const AppError =require("./../utils/appError")
-
+const AppError = require("./../utils/appError");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -17,7 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   res.status(201).json({
     status: "success",
-    token,      
+    token,
     data: {
       user: newUser,
     },
@@ -33,7 +33,6 @@ exports.login = catchAsync(async (req, res, next) => {
   }
   // 2) Check if user exists && password is correct
   const user = await User.findOne({ email }).select("+password");
- 
 
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError("Incorrect email or password", 401));
@@ -41,12 +40,44 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 3) If everything ok, send token to client
   //createSendToken(user, 200, req, res);
-  const token = signToken(user._id)
+  const token = signToken(user._id);
 
-  
   res.status(201).json({
     status: "success",
     token,
-   
-  }); 
+  });
+});
+
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+ 
+
+  } 
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  console.log(decoded,"jhfjg")
+
+
+  // 3) Check if user still exists
+ 
+
+  // 4) Check if user changed password after the token was issued
+  
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+
+  next();
 });
