d5beb366b89754f36095f71e3a8295d8bed4a9af
dileepnagankera <dileepnagankera@gmail.com>
2024-08-15 21:09:00 +0530

    NATR-56-  Updating the Current User Data

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const authController = require("./../controllers/authController");
 const router = express.Router();
+
 const {
   signup,
   login,
   forgotPassword,
   resetPassword,
   updatePassword,
+  
 } = require("../controllers/authController");
 const {
   getAllUsers,
@@ -14,17 +16,17 @@ const {
   createUser,
   updatUser,
   deleteUser,
-
- 
+  updateMe,
 } = require("../controllers/userController");
-router.get("/", getAllUsers);
+router.get("/", getAllUsers); 
 router.get("/:id", getUser);
 router.post("/", createUser);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
-router.patch("/updateMyPassword",authController.protect, updatePassword);
+router.patch("/updateMyPassword", authController.protect, updatePassword);
+router.patch("/updateMe", authController.protect, updateMe);
 router.put("/:id", updatUser);
 router.delete("/:id", deleteUser);
 
