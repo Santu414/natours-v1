89aad4a6abe6389adb8e4a6fe6b034f0fead0194
Santu414 <snagankera@gmail.com>
2024-08-15 12:38:06 +0530

    NATR-55 - fixed the issues

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,7 @@ const signToken = (id) => {
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
-  const token = signToken({ id: newUser._id });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: "success",
@@ -113,56 +113,59 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
 
   if (!user) {
-    return next(new AppError("There is no user with email address.", 404));
+    return next(new AppError("There is no user with that email address.", 404));
   }
 
   // 2) Generate the random reset token
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
 
-  // 3) send it to users email
-
-  const resetURL = `${req.protocol}://${req.get(
-    "host"
-  )}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH reqeust with your new password and PasswordConform to:${resetURL}.\nIf you didn't forget yout password, please ignore this email!`;
+  // 3) Send it to user's email
+  const resetURL = `${req.protocol}://${req.get("host")}/api/v1/users/resetPassword/${resetToken}`;
+  const message = `Forgot your password? Submit a PATCH request with your new password and password confirmation to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
     await sendEmail({
       email: user.email,
-      subject: "Your send to email!",
+      subject: "Your password reset token (valid for 10 min)",
       message,
     });
 
     res.status(200).json({
       status: "success",
-      message: "Token sent to email",
+      message: "Token sent to email!",
     });
   } catch (err) {
+    console.error("Error sending email:", err);  // Log the error for troubleshooting
+
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
     await user.save({ validateBeforeSave: false });
 
     return next(
-      new AppError("There was an error sending the email. Try again later!"),
-      500
+      new AppError("There was an error sending the email. Try again later!", 500)
     );
   }
 });
 
+
 exports.resetPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on the token
   const hashedToken = crypto
-    .createHash("sha256")
-    .update(req.params.token)
-    .digest("hex");
+  .createHash("sha256")
+  .update(req.params.token)
+  .digest("hex");
 
+   console.log("Hashed Token:", hashedToken);
+   
   const user = await User.findOne({
     passwordResetToken: hashedToken,
     passwordResetExpires: { $gt: Date.now() },
   });
+  console.log("User's Token:", user.passwordResetToken);
+  console.log("User's Expiry:", user.passwordResetExpires);
   // 2) If token has not expired, and there is user, set the new password
+  console.log("useruser",user)
   if (!user) {
     return next(new AppError("Token is invalid or has expired", 400));
   }
