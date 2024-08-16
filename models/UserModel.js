43362f88471e1ae7ff5a04de1a5438d8b3ba12cb
dileepnagankera <dileepnagankera@gmail.com>
2024-08-16 10:56:56 +0530

    NATR-57 - Deleting the Current User

--- models/UserModel.js
+++ models/UserModel.js
@@ -43,6 +43,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active:{
+    type:Boolean,
+    default:true,
+    select:false
+  }
 });
 
 // Pre-save hook to hash the password
@@ -69,6 +74,13 @@ userSchema.pre("save", function (next) {
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
+
+userSchema.pre(/^find/, function (next) {
+  // this points to the current query
+  this.find({ active: {$ne:false} });
+  next();
+});
+
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,7 +8,6 @@ const {
   forgotPassword,
   resetPassword,
   updatePassword,
-  
 } = require("../controllers/authController");
 const {
   getAllUsers,
@@ -17,8 +16,9 @@ const {
   updatUser,
   deleteUser,
   updateMe,
+  deleteMe,
 } = require("../controllers/userController");
-router.get("/", getAllUsers); 
+router.get("/", getAllUsers);
 router.get("/:id", getUser);
 router.post("/", createUser);
 router.post("/signup", signup);
@@ -27,6 +27,7 @@ router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
 router.patch("/updateMyPassword", authController.protect, updatePassword);
 router.patch("/updateMe", authController.protect, updateMe);
+router.delete("/deleteMe", authController.protect, deleteMe); 
 router.put("/:id", updatUser);
 router.delete("/:id", deleteUser);
 
