a4de212f1c7cb639d7e2cf6de45c3835ac964f3f
dileepnagankera <dileepnagankera@gmail.com>
2024-08-21 21:14:58 +0530

    NATR-69 - Virtual Populate Tours and Reviews

--- models/ToursModel.js
+++ models/ToursModel.js
@@ -13,110 +13,116 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       maxlength: [40, "A tour name must have less or equal then 40 characters"],
-      minlength: [10, "A tour name must have more or equal then 10 characters"]
+      minlength: [10, "A tour name must have more or equal then 10 characters"],
       // validate: [validator.isAlpha, 'Tour name must only contain caracters']
     },
     slug: String,
     duration: {
       type: Number,
-      required: [true, "A tour must have a duration"]
+      required: [true, "A tour must have a duration"],
     },
     maxGroupSize: {
       type: Number,
-      required: [true, "A tour must have a group size"]
+      required: [true, "A tour must have a group size"],
     },
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
       enum: {
         values: ["easy", "medium", "difficult"],
-        message: "Difficulty is either: easy, medium, difficulty"
-      }
+        message: "Difficulty is either: easy, medium, difficulty",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
       min: [1, "Rating must be above 1.0"],
       max: [5, "Rating must be below 5.0"],
-      set: val => Math.round(val * 10) / 10 
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
-      default: 0
+      default: 0,
     },
     price: {
       type: Number,
-      required: [true, "A tour must have a price"]
+      required: [true, "A tour must have a price"],
     },
     priceDiscount: {
       type: Number,
       validate: {
-        validator: function(value) {
+        validator: function (value) {
           // this only points to current doc on NEW documnet creation
           return value < this.price;
         },
-        message: "Discount price ({VALUE}) should be below regular price"
-      }
+        message: "Discount price ({VALUE}) should be below regular price",
+      },
     },
     summary: {
       type: String,
       trim: true,
-      required: [true, "A tour must have a description"]
+      required: [true, "A tour must have a description"],
     },
     description: {
       type: String,
-      trim: true
+      trim: true,
     },
     imageCover: {
       type: String,
-      required: [true, "A tour must have a cover image"]
+      required: [true, "A tour must have a cover image"],
     },
     images: [String],
     createdAt: {
       type: Date,
       default: Date.now(),
-      select: false
+      select: false,
     },
     startDates: [Date],
     secretTour: {
       type: Boolean,
-      default: false
+      default: false,
     },
     startLocation: {
       // GeoJSON
       type: {
         type: String,
         default: "Point",
-        enum: ["Point"]
+        enum: ["Point"],
       },
       coordinates: [Number],
       address: String,
-      description: String
+      description: String,
     },
     locations: [
       {
         type: {
           type: String,
           default: "Point",
-          enum: ["Point"]
+          enum: ["Point"],
         },
         coordinates: [Number],
         address: String,
         description: String,
-        day: Number
-      }
+        day: Number,
+      },
     ],
     guides: [
       {
         type: mongoose.Schema.ObjectId,
-        ref: "User"
-      }  
-    ]
+        ref: "User",
+      },
+    ],
+    reviews: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref:"Review"
+      },
+    ],
   },
-  
+
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
 );
 
@@ -126,11 +132,17 @@ tourSchema.virtual("durationWeeks").get(function() {
   return this.duration / 7;
 });
 
+//tourSchema.virtual("reviews",{
+ // ref:"Review",
+//  foreignField:"tour",
+ // localField:"_id"
+//})
+
 // Virtual populate
-tourSchema.virtual("reviews", {
+tourSchema.virtual("review", {
   ref: "Review",
   foreignField: "tour",
-  localField: "_id"
+  localField: "_id",
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() !.update()
